b901bdb66c8fa97abaec4ffdb6b345b82fd95a0f
hodongman <arshavin3@naver.com>
2018-07-10 00:50:20 +0900

    repository and security config update

---
+++ src/main/java/com/example/signal/config/AuditingConfig.java
@@ -0,0 +1,10 @@
+package com.example.signal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+    // 언젠간 쓸일이 있을것이다....
+}
---
+++ src/main/java/com/example/signal/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.example.signal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.signal.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Boolean isAdmin();
+}
